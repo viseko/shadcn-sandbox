7f11800c285b3b6478ce335a961ae6d744f6155f
Viktor Konovalov <viseko23@gmail.com>
2024-11-25 14:37:30 +0700

    Build: Update structure, and vite settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import App from './app';
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')).render(<App />);
